47f3b131180873572077d31bce95d9c42c6644ea
sanoj-s <sanojs@rapidvaluesolutions.com>
2022-11-09 21:47:30 +0530

    Updated the test cases

--- src/test/java/snj/selenium/testcases/APITesting.java
+++ src/test/java/snj/selenium/testcases/APITesting.java
@@ -3,7 +3,7 @@ package snj.selenium.testcases;
 import org.testng.annotations.Test;
 
 import com.snj.apihandler.APIActions;
-import com.snj.apihandler.APIValidationActions;
+import com.snj.apihandler.APIValidations;
 import com.snj.exception.AutomationException;
 
 import io.restassured.response.Response;
@@ -12,6 +12,6 @@ public class APITesting {
 	@Test(enabled = true)
 	public void TC001_sampleAPITestCase() throws AutomationException {
 		Response responseData = new APIActions().getRequest("https://reqres.in/", "api/users/2");
-		new APIValidationActions().validateStatusCode(responseData, 200);
+		new APIValidations().validateStatusCode(responseData, 200);
 	}
 }
--- src/test/java/snj/selenium/testcases/SampleTest.java
+++ src/test/java/snj/selenium/testcases/SampleTest.java
@@ -1,8 +1,11 @@
 package snj.selenium.testcases;
 
+import java.io.IOException;
+
 import org.testng.annotations.Test;
 
 import com.snj.action.UIActions;
+import com.snj.action.UtilityActions;
 import com.snj.action.WebActions;
 import com.snj.exception.AutomationException;
 import com.snj.reporting.AutomationReport;
@@ -16,15 +19,18 @@ public class SampleTest extends TestRunner {
 	}
 
 	@Test(enabled = true)
-	public void TC001_sampleTestCase() throws AutomationException {
+	public void TC001_sampleTestCase() throws AutomationException, IOException, InterruptedException {
 		new WebActions().loadWebApplication(driver, "https://www.google.com");
 		new UIActions().type(driver, SampleObjects.txt_searchbox, "JourneyOfQuality");
 		new AutomationReport().trackSteps("Data Entered");
 	}
 
 	@Test(enabled = true)
-	public void TC002_sampleTestCase() throws AutomationException {
+	public void TC002_sampleTestCase() throws AutomationException, IOException, InterruptedException {
 		new WebActions().loadWebApplication(driver, "https://automationbookstore.dev/");
+		new UtilityActions().collectLoadTime(driver, "//h2[@id='pid1_title']", 2, "Home", "AutomationBook");
+		new UtilityActions().startLighthouseAudit(driver.getCurrentUrl(), "performance,seo", "no");
+		new UtilityActions().startAccessibilityAudit(driver, "Home");
 //		new UIActions().click(driver, "li", "left", SampleObjects.btn_book);
 //		new UIActions().click(driver, "li", "right", SampleObjects.btn_book1);
 //		new UIActions().click(driver, "li", "above", SampleObjects.btn_book);
